e6e0be231e46554442df5cfd3425541bdc797b90
Andrew KeepCoding <andrewkeepcoding@gmail.com>
2024-11-20 17:24:26 +0900

    Add project files.

---
+++ ListViewWithNumberBoxesIssue/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+using Microsoft.UI.Xaml;
+
+namespace ListViewWithNumberBoxesIssue
+{
+    public sealed partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ ListViewWithNumberBoxesIssue/Shell.xaml.cs
@@ -0,0 +1,41 @@
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Microsoft.UI.Xaml.Controls;
+using System.Collections.ObjectModel;
+
+namespace ListViewWithNumberBoxesIssue;
+
+public partial class Item(int value, int min) : ObservableObject
+{
+    [ObservableProperty]
+    private int _value = value;
+
+    public int Min { get; } = min;
+}
+
+public partial class ShellViewModel : ObservableObject
+{
+    [ObservableProperty]
+    private ObservableCollection<Item> _items =
+        [
+        new Item(value: 1, min: 1),
+        new Item(value: 2, min: 2),
+        new Item(value: 3, min: 3),
+        ];
+
+    [RelayCommand]
+    private void MoveFirstToLast()
+    {
+        Items.Move(0, Items.Count - 1);
+    }
+}
+
+public partial class Shell : Page
+{
+    public Shell()
+    {
+        InitializeComponent();
+    }
+
+    public ShellViewModel ViewModel { get; } = new();
+}
